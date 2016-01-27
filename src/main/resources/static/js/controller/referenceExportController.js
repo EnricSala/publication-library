0496f984767c18672561d877913d1b64e5e488a3
Enric Sala <sinh32.enric@gmail.com>
2016-01-27 21:07:19 +0100

    Add reference export dialog, controller and styles, pending implement

---
+++ src/main/resources/static/js/controller/referenceExportController.js
@@ -0,0 +1,16 @@
+/*
+ * Reference export controller
+ */
+
+angular
+  .module('app.controllers')
+  .controller('ReferenceExportController', referenceExportController);
+
+function referenceExportController($scope, $mdDialog, init) {
+  $scope.publications = init;
+
+  $scope.close = function() {
+    $mdDialog.cancel();
+  }
+
+};
