f22acef7b415489e752999a8fcc33beec606b85b
Enric Sala <sinh32.enric@gmail.com>
2016-02-02 13:07:27 +0100

    Add PublisherRepository.findByType method

--- src/main/java/mcia/publications/repository/PublisherRepository.java
+++ src/main/java/mcia/publications/repository/PublisherRepository.java
@@ -1,9 +1,13 @@
 package mcia.publications.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import mcia.publications.domain.Publisher;
 
 public interface PublisherRepository extends PagingAndSortingRepository<Publisher, String> {
+
+	public List<Publisher> findByType(String type);
 
 }
