b9cd39c597784e4fd90898df3e1581413d205d71
Enric Sala <sinh32.enric@gmail.com>
2016-07-18 23:11:34 +0200

    Add initial version of publishers view and controller

--- web/src/app/controller/publishers.controller.js
+++ web/src/app/controller/publishers.controller.js
@@ -1,10 +1,20 @@
 class PublishersController {
 
-  constructor() {
+  constructor(Summary) {
+    this.Summary = Summary;
+    this.init();
+  }
 
+  init() {
+    this.Summary.publishers().then(
+      byType => {
+        this.journals = byType.journal;
+        this.conferences = byType.conference;
+        this.books = byType.book;
+      });
   }
 
 }
 
-PublishersController.$inject = [];
+PublishersController.$inject = ['Summary'];
 export default PublishersController;
