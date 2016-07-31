1933308799538bde8e823633e6424b53d356c636
Enric Sala <sinh32.enric@gmail.com>
2016-07-31 18:10:27 +0200

    Replace constructor of initial populator with lombok's required args constructor
     This is possible due to using Spring 4.3+ which no longer needs autowired annotations when there is only one constructor.

--- backend/src/main/java/mcia/publications/InitialPopulator.java
+++ backend/src/main/java/mcia/publications/InitialPopulator.java
@@ -1,18 +1,7 @@
 package mcia.publications;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Component;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
-
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import mcia.publications.domain.Author;
@@ -21,29 +10,31 @@ import mcia.publications.domain.Publisher;
 import mcia.publications.repository.AuthorRepository;
 import mcia.publications.repository.PublicationRepository;
 import mcia.publications.repository.PublisherRepository;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 @Component
 @ConfigurationProperties(prefix = "init")
+@RequiredArgsConstructor
 @Slf4j
 public class InitialPopulator implements ApplicationRunner {
 
-	private @Setter String publicationsFile = "";
-	private @Setter String authorsFile = "";
-	private @Setter String publishersFile = "";
+	private	@Setter String publicationsFile = "";
+	private	@Setter	String authorsFile = "";
+	private	@Setter	String publishersFile = "";
+
+	private final ObjectMapper mapper = new ObjectMapper();
 
 	private final PublicationRepository publicationRepository;
 	private final AuthorRepository authorRepository;
 	private final PublisherRepository publisherRepository;
-
-	private final ObjectMapper mapper = new ObjectMapper();
-
-	@Autowired
-	public InitialPopulator(PublicationRepository publicationRepository,
-			AuthorRepository authorRepository, PublisherRepository publisherRepository) {
-		this.publicationRepository = publicationRepository;
-		this.authorRepository = authorRepository;
-		this.publisherRepository = publisherRepository;
-	}
 
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
